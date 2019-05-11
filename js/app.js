72c264ed674451d7b8d296fbe2f249f8089babf7
chavez-japan <49377908+chavez-japan@users.noreply.github.com>
2019-05-11 00:05:18 -0700

    updated js navigation smoothness

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 $(document).ready(() => {
     $('#nav-button').click(() => {
-        if ($('#top-nav ul').css('display') == 'none')
-            $('#top-nav ul').css({'display': 'block'});
-        else
-            $('#top-nav ul').css({'display': 'none'});
+        $('#top-nav ul').slideToggle('slow'); 
     });
 });
